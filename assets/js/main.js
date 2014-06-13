05ca1e76aa7390229e4d7165227142d1fd18c117
vescoyez <valentin.escoyez@gmail.com>
2014-06-13 06:56:25 +0200

    new site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
-
+$(document).ready(function(){
+	
+	prettyPrint();
+	
+	$('.to-components').on('click', function(e){
+		e.preventDefault();
+		$(this).closest('ul').find('li').removeClass('active');
+		$(this).closest('li').addClass('active');
+		$(this).next('ul').addClass('open');
+	});
+	
+	$('.to-expand').on('click', function(e){
+		e.stopPropagation();
+		e.preventDefault();
+		$(this).find('.nav-icon').toggleClass('expand-icon collapse-icon');
+		$('body').toggleClass('menu-expanded');
+	});
+	
+	$(document).on('click', '.menu-expanded', function(){
+		$(this).removeClass('menu-expanded');
+		$('.collapse-icon').toggleClass('expand-icon collapse-icon');
+	});
+	
+	window.addEventListener('load', function() {
+		new FastClick(document.body);
+	}, false);
+});
